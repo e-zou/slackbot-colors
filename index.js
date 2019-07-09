b9aa3b2f39a980fb5e9bea06f0ca8b0514489c2b
e-zou <emily.zou2@gmail.com>
2019-07-09 12:23:50 -0400

    deployment with text and not pretext

--- index.js
+++ index.js
@@ -67,23 +67,23 @@ app.post('/post', (req, res) => {
         attachments: [
             {
                 color: "#edC560",
-                pretext: "Light Yellow: #EDC560",
+                text: "Light Yellow: #EDC560",
             },
             {
                 color: "#97C6BF",
-                pretext: "Leaf Green: #97C6BF",
+                text: "Leaf Green: #97C6BF",
             },
             {
                 color: "#F4C137",
-                pretext: "Dark Yellow: #F4C137",
+                text: "Dark Yellow: #F4C137",
             },
             {
                 color: "#FFB208",
-                pretext: "Light Orange: #FFB208",
+                text: "Light Orange: #FFB208",
             },
             {
                 color: "#E98035",
-                pretext: "Blood Orange: #E98035",
+                text: "Blood Orange: #E98035",
             },
             {
                 color: "#5F8D89",
@@ -91,15 +91,15 @@ app.post('/post', (req, res) => {
             },
             {
                 color: "#518991",
-                pretext: "Bluish Green: #518991",
+                text: "Bluish Green: #518991",
             },
             {
                 color: "#264044",
-                pretext: "Super Dark Green: #264044",
+                text: "Super Dark Green: #264044",
             },
             {
                 color: "#F0F2F6",
-                pretext: "Off-White: #F0F2F6",
+                text: "Off-White: #F0F2F6",
             }
         ]
     }
