f97b8fe85c5c4da1b5139fcc12fd154a3004bb3c
e-zou <emily.zou2@gmail.com>
2019-07-09 12:21:03 -0400

    deployment with first lowercased

--- index.js
+++ index.js
@@ -65,11 +65,42 @@ app.post('/post', (req, res) => {
         response_type: "in_channel",
         text: "X-mode's Branding Colors",
         attachments: [
-        {
-            color: "#EDC560",
-            pretext: "Light Yellow",
-        },
-
+            {
+                color: "#edC560",
+                pretext: "Light Yellow: #EDC560",
+            },
+            {
+                color: "#97C6BF",
+                pretext: "Leaf Green: #97C6BF",
+            },
+            {
+                color: "#F4C137",
+                pretext: "Dark Yellow: #F4C137",
+            },
+            {
+                color: "#FFB208",
+                pretext: "Light Orange: #FFB208",
+            },
+            {
+                color: "#E98035",
+                pretext: "Blood Orange: #E98035",
+            },
+            {
+                color: "#5F8D89",
+                pretext: "Dark Green: #5F8D89",
+            },
+            {
+                color: "#518991",
+                pretext: "Bluish Green: #518991",
+            },
+            {
+                color: "#264044",
+                pretext: "Super Dark Green: #264044",
+            },
+            {
+                color: "#F0F2F6",
+                pretext: "Off-White: #F0F2F6",
+            }
         ]
     }
 
